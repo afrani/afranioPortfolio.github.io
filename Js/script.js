450ebd16796d8bb86b9c40e1af0399ebcdbe566f
AFRANIO GUERRA <71418895+afrani@users.noreply.github.com>
2024-04-21 17:38:52 -0400

    upload do site do portfolio

---
+++ Js/script.js
@@ -0,0 +1,10 @@
+function abrirmenu (){
+let barraMenuaberto = document.getElementById ('barra-menu')
+
+if (barraMenuaberto.style.width === 'opx') {
+    barraMenuaberto.style.width = '180px';
+
+}else{
+     barraMenuaberto.style.width = 'opx';
+}
+}
